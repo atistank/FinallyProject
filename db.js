de2049e4a673017bca92df1dcb047ec3c496a582
atistank <atistank95@gmail.com>
2018-01-21 11:52:06 +0700

    aasd

---
+++ db.js
@@ -0,0 +1,42 @@
+const sequelize = require('sequelize')
+
+const db = new sequelize({
+
+    database: "d5rvn4isku8tlu",
+    username: "adyssuakmijxbm",
+    password: "2200dd5691094abe8dc7faed9b50c4713756e8a57a93036433c401e0a61f62a0",
+    host: "ec2-107-21-236-219.compute-1.amazonaws.com",
+    port: 5432,
+    dialect: 'postgres'
+})
+
+// ham kiem tra test ket noi
+db.authenticate()
+  .then( function(){ console.log( 'ket noi thanh cong' )})
+  .catch(err => console.log( err.message) )
+// dinh nghia table
+const User = db.define('User',
+{
+    username: sequelize.STRING,
+    email: sequelize.STRING,
+    password: sequelize.STRING,
+    avatar: sequelize.STRING,
+    cover: sequelize.STRING,
+    quyenhan: sequelize.INTEGER,
+    trangthai: sequelize.INTEGER,
+    like: sequelize.INTEGER,
+    userid: {
+      type: sequelize.INTEGER,
+      primaryKey: true,
+      unique: true,
+      allowNull: false,
+      autoIncrement: true
+    }
+  }, 
+  {
+    freezeTableName: true
+
+})
+db.sync({force: true})
+
+module.exports = db.models
---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const models = require('./db.js')
+
+app.use(bodyParser.urlencoded({extended: true}))
+// cau hinh root
+
+
+app.get('/',(req,res)=> res.send('wellcome web cua finally Project'))
+
+
+
+// //read one
+// app.get('/login', (req, res) => {
+//     let {email,password} = req.query
+//     models.User.findById(email)
+//       .then(users => res.json({ketqua: 1, data: users}))
+//       .catch(() => res.json({ketqua: 0}))
+//   })
+
+
+// read
+app.get('/user',(req,res)=> {
+    models.User.findAll()
+    .then(users => res.json({ketqua: 1, data: users}))
+    .catch(() => res.json({ketqua: 0}))
+
+})
+
+// them user
+app.post('/add_user',(req,res)=> {
+let {username,email,password,avatar,cover,quyenhan,trangthai,like} = req.body
+models.User.create({
+     username,
+     email,
+     password,
+     avatar,
+     cover,
+     quyenhan,
+     trangthai,
+     like
+          
+})
+.then(() => res.json({ketqua: 1}))
+.catch(() => res.json({ketqua: 0}))
+})
+
+
+
+
+// cap nhat user
+app.post('/update_user',(req,res)=>{
+let {userid,username,email,password,avatar,cover,quyenhan,trangthai,like} = req.body
+   models.User.update({
+            username,
+            email,
+            password,
+            avatar,
+            cover,
+            quyenhan,
+            trangthai,
+            like
+        },{ where: {userid},returning: true })
+.then(row => res.json({ketqua: 1, rowsCount: row[0], data: row[1]  }))
+.catch(err => res.json({ketqua: 0, error: err.message} ))
+
+})//
+
+
+
+
+
+
+app.post('/delete',(req,res)=>{
+    let {id} = req.body
+     models.User.destroy({
+        where: {userid: id}
+    })
+    .then( row => res.json( {ketqua: 1, rowsCount: row}) )
+    .catch(err => res.json( {ketqua: 0, error: err.message} ))
+
+})
+
+//tao port lang nghe cac ket noi
+
+const port = process.env.PORT || 3000
+app.listen(port, function(){ console.log(`server started on port ${port}`) })
