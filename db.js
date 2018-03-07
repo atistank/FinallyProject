56d0ef56ce84b5dee875b6f41dd53d09c6118e3d
atistank <atistank95@gmail.com>
2018-03-07 22:21:24 +0700

    xog 40

--- db.js
+++ db.js
@@ -27,7 +27,6 @@ const User = db.define('UserData',
     cover: sequelize.STRING,
     quyenhan: sequelize.INTEGER,
     trangthai: sequelize.INTEGER,
-    like: sequelize.INTEGER,
     userid: {
       type: sequelize.INTEGER,
       primaryKey: true,
@@ -75,16 +74,16 @@ const Data = db.define('Khoaluan4',
 const klcv_bomon = db.define('klcv_bomon',
 {
   id_bomon: {
-      type: sequelize.STRING,
+      type: sequelize.varchar(25),
       primaryKey: true,
       unique: true,
       allowNull: false
     },ten_bomon: {
-      type: sequelize.STRING,
-      allowNull: true
+      type: sequelize.varchar(255),
+      allowNull: false
     },id_khoa: {
-      type: sequelize.STRING,
-      allowNull: true
+      type: sequelize.varchar(255),
+      allowNull: false
     }
    
   }, 
@@ -94,10 +93,736 @@ const klcv_bomon = db.define('klcv_bomon',
 
 })
 
+const klcv_chitiet_cv = db.define('klcv_chitiet_cv',
+{
+  ID_GiangVien: {
+      type: sequelize.varchar(25),
+      primaryKey: true,
+      unique: true,
+      allowNull: false
+    },
+    ID_ChucVu: {
+      type: sequelize.varchar(25),
+      allowNull: false
+    }
+  }, 
+  {
+    timestamps: false,
+    freezeTableName: true
+})
+
+const klcv_chitiet_hdcm = db.define('klcv_chitiet_hdcm',
+{
+  
+    ID_GiangVien: {
+      type: sequelize.STRING(25),
+      allowNull: false
+    },
+    ID_HDCM: {
+      type: sequelize.STRING(25),
+      allowNull: false
+    },
+    SoLuong: {
+      type: sequelize.INTEGER(11),
+      allowNull: false
+    },
+    ID: {
+      type: sequelize.INTEGER(11),
+      primaryKey: true,
+      unique: true,
+      allowNull: false
+    },
+    ID_BoMon: {
+      type: sequelize.STRING(25),
+      allowNull: false
+    }
+  }, 
+  {
+    timestamps: false,
+    freezeTableName: true
+})
+
+
+const klcv_chitiet_hdkhcn = db.define('klcv_chitiet_hdkhcn',
+{
+  
+    ID_GiangVien: {
+      type: sequelize.varchar(25),
+      allowNull: false
+    },
+    ID_KHCN: {
+      type: sequelize.varchar(25),
+      allowNull: false
+    },
+    
+    NoiDung: {
+      type: sequelize.varchar(255),
+      allowNull: false
+    },
+    ID: {
+      type: sequelize.INTEGER(11),
+      primaryKey: true,
+      unique: true,
+      allowNull: false
+    },
+    QuyChuan: {
+      type: sequelize.INTEGER(11),
+      allowNull: false
+    },
+    GhiChu: {
+      type: sequelize.varchar(255),
+      allowNull: true
+    }
+  }, 
+  {
+    timestamps: false,
+    freezeTableName: true
+})
+
+
+//
+const klcv_chucvu = db.define('klcv_chucvu',
+{
+  ID_ChucVu: {
+      type: sequelize.INTEGER(11),
+      primaryKey: true,
+      unique: true,
+      allowNull: false
+    },Ten_ChucVu: {
+      type: sequelize.varchar(255),
+      allowNull: false
+    },TL_GiamTruGGD: {
+      type: sequelize.FLOAT,
+      allowNull: false
+    }
+   
+  }, 
+  {
+    timestamps: false,
+    freezeTableName: true
+
+})
+
+const klcv_giangvien = db.define('klcv_giangvien',
+{
+  ID_GiangVien: {
+      type: sequelize.varchar(25),
+      unique: true,
+      allowNull: false
+    },Ho_GiangVien: {
+      type: sequelize.varchar(45),
+      allowNull: false
+    }
+    ,Ten_GiangVien: {
+      type: sequelize.varchar(45),
+      allowNull: false
+    }
+    ,GioiTinh: {
+      type: sequelize.varchar(25),
+      allowNull: true
+    }
+    ,Ngach: {
+      type: sequelize.varchar(25),
+      allowNull: false
+    }
+    ,ChucVu: {
+      type: sequelize.varchar(25),
+      allowNull: false
+    }
+    ,ID_BoMon: {
+      type: sequelize.varchar(25),
+      allowNull: false
+    },
+    ID: {
+      type: sequelize.INTEGER(11),
+      primaryKey: true,
+      unique: true,
+      allowNull: false
+    } 
+  }, 
+  {
+    timestamps: false,
+    freezeTableName: true
+})
+
+
+const klcv_hdcm = db.define('klcv_hdcm',
+{
+  ID_HDCM: {
+      type: sequelize.INTEGER(25),
+      primaryKey: true,
+      unique: true,
+      allowNull: false
+    },NoiDung: {
+      type: sequelize.TEXT
+    }
+    ,DonViTinh: {
+      type: sequelize.varchar(45),
+      allowNull: false,
+      default: null
+    }
+    ,QuyChuan: {
+      type: sequelize.FLOAT,
+      allowNull: false,
+      default: null
+    }
+    ,GhiChu: {
+      type: sequelize.varchar(255),
+      allowNull: true,
+      default: null
+    }
+    
+  }, 
+  {
+    timestamps: false,
+    freezeTableName: true
+})
+
+
+
+const klcv_hdgd = db.define('klcv_hdgd',
+{
+  ID_HDGD: {
+      type: sequelize.varchar(25),
+      primaryKey: true,
+      unique: true,
+      allowNull: false
+    }
+    ,Nhom_HP: {
+      type: sequelize.varchar(45),
+      allowNull: false,
+      default: null
+    }
+    ,ToThucHanh: {
+      type: sequelize.varchar(45),
+      allowNull: false,
+      default: "0"
+    }
+    ,Thu: {
+      type: sequelize.INTEGER(11),
+      allowNull: false,
+      default: null
+    }
+    ,TietBD: {
+      type: sequelize.INTEGER(11),
+      allowNull: false,
+      default: null
+    }
+    ,ID_GiangVien: {
+      type: sequelize.varchar(25),
+      allowNull: false
+    }
+    ,Phong: {
+      type: sequelize.varchar(45),
+      allowNull: false,
+      default: null
+    }
+    ,SiSo: {
+      type: sequelize.INTEGER(11),
+      allowNull: false,
+      default: null
+    }
+    ,Lop: {
+      type: sequelize.varchar(45),
+      allowNull: false,
+      default: null
+    }
+    ,SoTietThucHien: {
+      type: sequelize.INTEGER(11),
+      allowNull: false,
+      default: null
+    }
+    ,HocKy: {
+      type: sequelize.varchar(25),
+      allowNull: false,
+      default: null
+    }
+    ,ID: {
+      type: sequelize.INTEGER(11),
+      allowNull: false,
+      default: null
+    }
+    
+  }, 
+  {
+    timestamps: false,
+    freezeTableName: true
+})
+
+
+
+const klcv_hdkhcn = db.define('klcv_hdkhcn',
+{
+  ID_KHCN: {
+      type: sequelize.INTEGER(25),
+      primaryKey: true,
+      unique: true,
+      allowNull: false
+    }
+    ,NoiDung: {
+      type: sequelize.TEXT
+    }
+    ,QuyChuan: {
+      type: sequelize.FLOAT,
+      allowNull: false,
+      default: null
+    }
+    ,GhiChu: {
+      type: sequelize.TEXT,
+      allowNull: true
+    }
+    
+  }, 
+  {
+    timestamps: false,
+    freezeTableName: true
+})
+
+const klcv_heso = db.define('klcv_heso',
+{
+  ID: {
+      type: sequelize.INTEGER(11),
+      primaryKey: true,
+      unique: true,
+      allowNull: false
+    }
+    ,HeSo_LyThuyet: {
+      type: sequelize.FLOAT,
+      allowNull: false,
+      default: null
+    }
+    ,HeSo_LyThuyet_TD: {
+      type: sequelize.FLOAT,
+      allowNull: false,
+      default: null
+    }
+    ,HeSo_ThucHanh_TD: {
+      type: sequelize.FLOAT,
+      allowNull: false,
+      default: null
+    }
+    ,HeSo_NgoaiGio_LT: {
+      type: sequelize.FLOAT,
+      allowNull: false,
+      default: null
+    }
+    ,HeSo_LopDong_50: {
+      type: sequelize.FLOAT,
+      allowNull: false,
+      default: null
+    }
+    ,HeSo_LopDong_65: {
+      type: sequelize.FLOAT,
+      allowNull: false,
+      default: null
+    }
+    ,HeSo_LopDong_80: {
+      type: sequelize.FLOAT,
+      allowNull: false,
+      default: null
+    }
+    ,HeSo_LopDong_100: {
+      type: sequelize.FLOAT,
+      allowNull: false,
+      default: null
+    }
+    ,HeSo_LopDong_120: {
+      type: sequelize.FLOAT,
+      allowNull: false,
+      default: null
+    }
+    ,HeSo_LopDong_140: {
+      type: sequelize.FLOAT,
+      allowNull: false,
+      default: null
+    }
+    
+    ,HeSo_LopDong_160: {
+      type: sequelize.FLOAT,
+      allowNull: false,
+      default: null
+    }
+    ,HeSo_LopDong_180: {
+      type: sequelize.FLOAT,
+      allowNull: false,
+      default: null
+    }
+    ,HeSo_LopDong_181: {
+      type: sequelize.FLOAT,
+      allowNull: false,
+      default: null
+    }
+    ,HeSo_NgoaiGio_TH: {
+      type: sequelize.FLOAT,
+      allowNull: false,
+      default: null
+    }
+
+  }, 
+  {
+    timestamps: false,
+    freezeTableName: true
+})
+
+const klcv_hocphan = db.define('klcv_hocphan',
+{
+  ID_HocPhan: {
+      type: sequelize.varchar(25),
+      primaryKey: true,
+      unique: true,
+      allowNull: false
+    }
+    ,Ten_HocPhan: {
+      type: sequelize.varchar(255),
+      allowNull: false,
+      default: null
+    }
+    ,SoTinChi: {
+      type: sequelize.INTEGER(11),
+      allowNull: false
+    }
+    ,SoTietLyThuyet: {
+      type: sequelize.INTEGER(11),
+      allowNull: false
+    }
+    ,SoTietThucHanh: {
+      type: sequelize.INTEGER(11),
+      allowNull: false
+    }
+  }, 
+  {
+    timestamps: false,
+    freezeTableName: true
+})
+
+
+const klcv_ketqua = db.define('klcv_ketqua',
+{
+  ID_GiangVien: {
+      type: sequelize.varchar(25),
+      primaryKey: true,
+      unique: true,
+      allowNull: false
+    }
+    ,DMGD: {
+      type: sequelize.FLOAT,
+      allowNull: false,
+      default: null
+    }
+    ,DMKH: {
+      type: sequelize.FLOAT,
+      allowNull: false,
+      default: null
+    }
+    ,TaiChuc: {
+      type: sequelize.FLOAT,
+      allowNull: false,
+      default: null
+    }
+    ,ChinhQuy: {
+      type: sequelize.FLOAT,
+      allowNull: false,
+      default: null
+    }
+    ,HDCM: {
+      type: sequelize.FLOAT,
+      allowNull: false,
+      default: null
+    }
+    ,HDKH: {
+      type: sequelize.FLOAT,
+      allowNull: false,
+      default: null
+    }
+
+    ,ID_BoMon: {
+      type: sequelize.varchar(25),
+      allowNull: false
+    }
+    ,K: {
+      type: sequelize.INTEGER(11),
+      allowNull: false
+    }
+    ,ThuaGio: {
+      type: sequelize.FLOAT,
+      allowNull: false,
+      default: null
+    }
+    ,Luong: {
+      type: sequelize.INTEGER(11),
+      allowNull: false,
+      default: "50000000"
+    }
+    ,HeSo: {
+      type: sequelize.FLOAT,
+      allowNull: false,
+      default: null
+    }
+  }, 
+  {
+    timestamps: false,
+    freezeTableName: true
+})
+
+const klcv_khoa = db.define('klcv_khoa',
+{
+  ID_Khoa: {
+      type: sequelize.varchar(25),
+      primaryKey: true,
+      unique: true,
+      allowNull: false
+    }
+    ,Ten_Khoa: {
+      type: sequelize.varchar(255),
+      allowNull: false,
+      default: null
+    }
+},
+{
+    timestamps: false,
+    freezeTableName: true
+})
+
+
+
+const klcv_luong = db.define('klcv_luong',
+{
+  ID: {
+      type: sequelize.INTEGER(11),
+      primaryKey: true,
+      unique: true,
+      allowNull: false
+    }
+    ,TyLe: {
+      type: sequelize.FLOAT,
+      allowNull: false,
+      default: null
+    }
+    ,DonGia: {
+      type: sequelize.INTEGER(11),
+      allowNull: false,
+      default: null
+    }
+},
+{
+    timestamps: false,
+    freezeTableName: true
+})
+
+const klcv_namhoc = db.define('klcv_namhoc',
+{
+  ID_NamHoc: {
+      type: sequelize.varchar(25),
+      primaryKey: true,
+      unique: true,
+      allowNull: false
+    }
+    ,Ten_NamHoc: {
+      type: sequelize.varchar(25),
+      allowNull: false,
+      default: null
+    }
+},
+{
+    timestamps: false,
+    freezeTableName: true
+})
+
+const klcv_ngach = db.define('klcv_ngach',
+{
+  ID_Ngach: {
+      type: sequelize.INTEGER(11),
+      primaryKey: true,
+      unique: true,
+      allowNull: false
+    }
+    ,Ten_Ngach: {
+      type: sequelize.varchar(255),
+      allowNull: false,
+      default: null
+    }
+    ,DinhMuc_GD: {
+      type: sequelize.INTEGER(11),
+      allowNull: false,
+      default: null
+    }
+    ,DinhMuc_NCKH: {
+      type: sequelize.INTEGER(11),
+      allowNull: false,
+      default: null
+    }
+    ,DinhMuc_HDK: {
+      type: sequelize.INTEGER(11),
+      allowNull: false,
+      default: null
+    }
+},
+{
+    timestamps: false,
+    freezeTableName: true
+})
+
+const klcv_nhatky = db.define('klcv_nhatky',
+{
+  ID: {
+      type: sequelize.INTEGER(11),
+      primaryKey: true,
+      unique: true,
+      allowNull: false
+    }
+    ,TaiKhoan: {
+      type: sequelize.varchar(45),
+      allowNull: false,
+      default: null
+    }
+    ,TenTaiKhoan: {
+      type: sequelize.varchar(45),
+      allowNull: false,
+      default: null
+    }
+    ,Ngay: {
+      type: sequelize.varchar(45),
+      allowNull: false,
+      default: null
+    }
+    ,Gio: {
+      type: sequelize.varchar(45),
+      allowNull: false,
+      default: null
+    }
+    ,HanhDong: {
+      type: sequelize.varchar(45),
+      allowNull: false,
+      default: null
+    }
+    ,ThamChieu: {
+      type: sequelize.varchar(45),
+      allowNull: false,
+      default: null
+    }
+},
+{
+    timestamps: false,
+    freezeTableName: true
+})
+
+const klcv_phanhoi = db.define('klcv_phanhoi',
+{
+  ID_PhanHoi: {
+      type: sequelize.INTEGER(11),
+      primaryKey: true,
+      unique: true,
+      allowNull: false
+    }
+    ,ID_GiangVien: {
+      type: sequelize.varchar(25),
+      allowNull: true,
+      default: null
+    }
+    ,TieuDe: {
+      type: sequelize.varchar(255),
+      allowNull: true,
+      default: null
+    }
+    ,NoiDung: {
+      type: sequelize.TEXT,
+      allowNull: true,
+      default: null
+    }
+    ,TinhTrang: {
+      type: sequelize.INTEGER(11),
+      allowNull: false,
+      default: "0"
+    }
+},
+{
+    timestamps: false,
+    freezeTableName: true
+})
+
+
+
+
+const klcv_quyen = db.define('klcv_quyen',
+{
+  ID_Quyen: {
+      type: sequelize.INTEGER(11),
+      primaryKey: true,
+      unique: true,
+      allowNull: false
+    }
+    ,Ten_Quyen: {
+      type: sequelize.varchar(255),
+      allowNull: false,
+      default: null
+    }
+},
+{
+    timestamps: false,
+    freezeTableName: true
+})
+
+
+
+const klcv_thanhvien = db.define('klcv_thanhvien',
+{
+  ma_tv: {
+      type: sequelize.INTEGER(11),
+      primaryKey: true,
+      unique: true,
+      allowNull: false
+    }
+    ,ten_tv: {
+      type: sequelize.varchar(255),
+      allowNull: false,
+      default: null
+    }
+    ,taikhoan: {
+      type: sequelize.varchar(255),
+      allowNull: false,
+      default: null
+    }
+    ,matkhau: {
+      type: sequelize.varchar(255),
+      allowNull: false,
+      default: null
+    }
+    ,quyen: {
+      type: sequelize.INTEGER(11),
+      allowNull: false,
+      default: "2"
+    }
+
+},
+{
+    timestamps: false,
+    freezeTableName: true
+})
+
+
 db.sync({force: true})
 
 module.exports = {
   User,
   Data,
-  klcv_bomon
+  klcv_bomon,
+klcv_chitiet_cv,
+klcv_chitiet_hdcm
+,klcv_chitiet_hdkhcn
+,klcv_chucvu
+,klcv_giangvien
+,klcv_hdcm
+,klcv_hdgd
+,klcv_hdkhcn
+,klcv_heso
+,klcv_hocphan
+,klcv_ketqua
+,klcv_khoa
+,klcv_luong
+,klcv_namhoc
+,klcv_ngach
+,klcv_nhatky
+,klcv_phanhoi
+,klcv_quyen
+,klcv_thanhvien
 }
--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const {User, Data, klcv_bomon}  = require('./db.js')
+const {User,
+    Data,
+    klcv_bomon,
+  klcv_chitiet_cv
+  ,klcv_chitiet_hdcm
+  ,klcv_chitiet_hdkhcn
+  ,klcv_chucvu
+  ,klcv_giangvien
+  ,klcv_hdcm
+  ,klcv_hdgd
+  ,klcv_hdkhcn
+  ,klcv_heso
+  ,klcv_hocphan
+  ,klcv_ketqua
+  ,klcv_khoa
+  ,klcv_luong
+  ,klcv_namhoc
+  ,klcv_ngach
+  ,klcv_nhatky
+  ,klcv_phanhoi
+  ,klcv_quyen
+  ,klcv_thanhvien}  = require('./db.js')
 
 app.use(bodyParser.urlencoded({extended: true}))
 // cau hinh root
@@ -21,32 +42,131 @@ app.get('/',(req,res)=> res.send('wellcome web cua finally Project'))
 
 
 // read
-app.get('/user2',(req,res)=> {
+app.get('/user',(req,res)=> {
     User.findAll()
     .then(users => res.json({ketqua: 1, data: users}))
     .catch(() => res.json({ketqua: 0}))
 
 })
-app.get('/test',(req,res)=> {
+app.get('/klcv_bomon',(req,res)=> {
     klcv_bomon.findAll()
     .then(klcv_bomon => res.json({ketqua: 1, data: klcv_bomon}))
     .catch(() => res.json({ketqua: 0}))
+})
 
+app.get('/klcv_chitiet_cv',(req,res)=> {
+    klcv_chitiet_cv.findAll()
+    .then(klcv_bomon => res.json({ketqua: 1, data: klcv_bomon}))
+    .catch(() => res.json({ketqua: 0}))
+})
+app.get('/klcv_chitiet_hdcm',(req,res)=> {
+    klcv_chitiet_hdcm.findAll()
+    .then(klcv_bomon => res.json({ketqua: 1, data: klcv_bomon}))
+    .catch(() => res.json({ketqua: 0}))
+})
+app.get('/klcv_chitiet_hdkhcn',(req,res)=> {
+    klcv_chitiet_hdkhcn.findAll()
+    .then(klcv_bomon => res.json({ketqua: 1, data: klcv_bomon}))
+    .catch(() => res.json({ketqua: 0}))
+})
+app.get('/klcv_chucvu',(req,res)=> {
+    klcv_chucvu.findAll()
+    .then(klcv_bomon => res.json({ketqua: 1, data: klcv_bomon}))
+    .catch(() => res.json({ketqua: 0}))
+})
+app.get('/klcv_giangvien',(req,res)=> {
+    klcv_giangvien.findAll()
+    .then(klcv_bomon => res.json({ketqua: 1, data: klcv_bomon}))
+    .catch(() => res.json({ketqua: 0}))
+})
+app.get('/klcv_hdcm',(req,res)=> {
+    klcv_hdcm.findAll()
+    .then(klcv_bomon => res.json({ketqua: 1, data: klcv_bomon}))
+    .catch(() => res.json({ketqua: 0}))
 })
 
 
-
-
-app.get('/data',(req,res)=> {
-    Data.findAll()
-    .then(khoaluan5 => res.json({
+app.get('/klcv_hdgd',(req,res)=> {
+    klcv_hdgd.findAll()
+    .then(klcv_hdgd => res.json({
         ketqua: 1,
-        data: khoaluan5
+        data: klcv_hdgd
     }))
     .catch(() => res.json({ketqua: 0}))
 
 })
 
+app.get('/klcv_hdkhcn',(req,res)=> {
+    klcv_hdkhcn.findAll()
+    .then(klcv_bomon => res.json({ketqua: 1, data: klcv_bomon}))
+    .catch(() => res.json({ketqua: 0}))
+})
+app.get('/klcv_heso',(req,res)=> {
+    klcv_heso.findAll()
+    .then(klcv_bomon => res.json({ketqua: 1, data: klcv_bomon}))
+    .catch(() => res.json({ketqua: 0}))
+})
+
+app.get('/klcv_hocphan',(req,res)=> {
+    klcv_hocphan.findAll()
+    .then(klcv_bomon => res.json({ketqua: 1, data: klcv_bomon}))
+    .catch(() => res.json({ketqua: 0}))
+})
+app.get('/klcv_heso',(req,res)=> {
+    klcv_heso.findAll()
+    .then(klcv_bomon => res.json({ketqua: 1, data: klcv_bomon}))
+    .catch(() => res.json({ketqua: 0}))
+})
+app.get('/klcv_ketqua',(req,res)=> {
+    klcv_ketqua.findAll()
+    .then(klcv_bomon => res.json({ketqua: 1, data: klcv_bomon}))
+    .catch(() => res.json({ketqua: 0}))
+})
+app.get('/klcv_khoa',(req,res)=> {
+    klcv_khoa.findAll()
+    .then(klcv_bomon => res.json({ketqua: 1, data: klcv_bomon}))
+    .catch(() => res.json({ketqua: 0}))
+})
+app.get('/klcv_luong',(req,res)=> {
+    klcv_luong.findAll()
+    .then(klcv_bomon => res.json({ketqua: 1, data: klcv_bomon}))
+    .catch(() => res.json({ketqua: 0}))
+})
+app.get('/klcv_khoa',(req,res)=> {
+    klcv_khoa.findAll()
+    .then(klcv_bomon => res.json({ketqua: 1, data: klcv_bomon}))
+    .catch(() => res.json({ketqua: 0}))
+})
+app.get('/klcv_namhoc',(req,res)=> {
+    klcv_namhoc.findAll()
+    .then(klcv_bomon => res.json({ketqua: 1, data: klcv_bomon}))
+    .catch(() => res.json({ketqua: 0}))
+})
+app.get('/klcv_ngach',(req,res)=> {
+    klcv_ngach.findAll()
+    .then(klcv_ngach => res.json({ketqua: 1, data: klcv_ngach}))
+    .catch(() => res.json({ketqua: 0}))
+})
+app.get('/klcv_nhatky',(req,res)=> {
+    klcv_nhatky.findAll()
+    .then(klcv_nhatky => res.json({ketqua: 1, data: klcv_nhatky}))
+    .catch(() => res.json({ketqua: 0}))
+})
+app.get('/klcv_phanhoi',(req,res)=> {
+    klcv_phanhoi.findAll()
+    .then(klcv_phanhoi => res.json({ketqua: 1, data: klcv_phanhoi}))
+    .catch(() => res.json({ketqua: 0}))
+})
+app.get('/klcv_quyen',(req,res)=> {
+    klcv_quyen.findAll()
+    .then(klcv_quyen => res.json({ketqua: 1, data: klcv_quyen}))
+    .catch(() => res.json({ketqua: 0}))
+})
+app.get('/klcv_thanhvien',(req,res)=> {
+    klcv_thanhvien.findAll()
+    .then(klcv_thanhvien => res.json({ketqua: 1, data: klcv_thanhvien}))
+    .catch(() => res.json({ketqua: 0}))
+})
 
 // them user
 app.post('/add_user',(req,res)=> {
