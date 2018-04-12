76a563b9f05e7284cad14e58f04611a0f91f5c4f
atistank <atistank95@gmail.com>
2018-04-12 09:36:32 +0700

    sdsd

--- db.js
+++ db.js
@@ -792,11 +792,17 @@ klcv_hdgd.belongsTo(klcv_giangvien, { foreignKey: 'ID_GiangVien',"through": {
 },constraints: false}) 
 klcv_giangvien.hasMany(klcv_hdgd, { foreignKey: 'ID_GiangVien'})
 
-klcv_hocphan.belongsTo(klcv_hdgd, { foreignKey: 'ID_HocPhan',"through": {
+klcv_hdgd.belongsTo(klcv_hocphan, { foreignKey: 'ID_HDGD',"through": {
   model: "chitiec",
   unique: false
 },constraints: false}) 
-klcv_hdgd.hasMany(klcv_hocphan, { foreignKey: 'ID_HDGD'})
+klcv_hocphan.hasMany(klcv_hdgd, { foreignKey: 'ID_HocPhan'})
+
+// klcv_hocphan.belongsTo(klcv_hdgd, { foreignKey: 'ID_HocPhan',"through": {
+//   model: "chitiec",
+//   unique: false
+// },constraints: false}) 
+// klcv_hdgd.hasMany(klcv_hocphan, { foreignKey: 'ID_HDGD'})
 
 //  db.sync({force: true})
 db.sync()
