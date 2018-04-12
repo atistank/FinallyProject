126ea63f9486da1b3d8c9d5e6d6a9a0b3dcf201d
atistank <atistank95@gmail.com>
2018-04-12 09:51:53 +0700

    dfdf

--- db.js
+++ db.js
@@ -804,8 +804,8 @@ klcv_hocphan.hasMany(klcv_hdgd, { foreignKey: 'ID_HDGD'})
 // },constraints: false}) 
 // klcv_hdgd.hasMany(klcv_hocphan, { foreignKey: 'ID_HDGD'})
 //
-//  db.sync({force: true})
-db.sync()
+ db.sync({force: true})
+// db.sync()
 module.exports = {
   User,Data,klcv_bomon,klcv_chitiet_hdcm,klcv_chitiet_hdkhcn,klcv_chucvu,klcv_giangvien,klcv_hdcm
 ,klcv_hdgd
